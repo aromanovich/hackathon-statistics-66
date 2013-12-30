cb2e7fd86e62911597d1f2b288ab5cb93e1b6ad0
Anton Romanovich <anthony.romanovich@gmail.com>
2013-12-30 12:38:23 +0600

    Smallfix

--- statistics/forms.py
+++ statistics/forms.py
@@ -7,7 +7,7 @@ class SurveyForm(wtf.Form):
     first_name = wtforms.TextField(u'Имя', [wtforms.validators.Required()])
     last_name = wtforms.TextField(u'Фамилия', [wtforms.validators.Required()])
     birthdate = wtforms.DateField(u'Дата рождения',
-            description=u'в формате 1990-14-08')
+            description=u'в формате 1990-08-14')
     height = wtforms.IntegerField(u'Рост', [wtforms.validators.Required()], description=u'в сантиметрах')
     eye_color = wtforms.SelectField(u'Цвет глаз', [wtforms.validators.Required()],
             choices=[('white', u'Белый'), ('black', u'Чёрный'), ('red', u'Красный')])
